cc7009307a261dff556ca9ed929ba218f0e979bf
Kevin Ruan Soares <contato@kevinrsoares.com.br>
2024-06-21 01:21:17 -0300

    chore: added env;

--- web/src/api/index.ts
+++ web/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: 'https://api.codechallenge.kevinsoares.com.br',
 });
 
 export default axiosInstance;
